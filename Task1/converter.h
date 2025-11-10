fd2050e88be9ef20bd83ff34ab0006802c86c012
Ildarc4S <ildarc4S@gmail.com>
2025-11-10 22:19:16 +0300

    Complete converter from BIN to HEX. Issues #1

--- Task1/converter.h
+++ Task1/converter.h
@@ -2,10 +2,12 @@
 #define CONVERTER
 
 #include <stdbool.h>
+#include <stdint.h>
 
 #define ERROR_MESSAGE_LEN 256
 #define BYTE_SIZE 1
 #define BYTE_COUNT 1
+#define FILE_FORMAT_LEN 4 // .bin or .hex
 
 typedef struct {
   bool convert_to_hex;  // flag -b
@@ -23,10 +25,18 @@ typedef struct {
 } HexPair;
 
 ConverterConfig* GetConfig();
+void CleanConfig();
+
 bool ParseArguments(int argc, char **argv);
+bool GenerateOutputFilename();
+
+void PrintHelp(const char *program_name);
+
+
 int CharToHex(char symbol);
-bool HexToByte(HexPair pair, unsigned char *result_byte);
+bool HexToByte(HexPair pair, uint8_t *result_byte);
 bool WriteHexToBin(const char* input_file, const char* output_file);
+bool WriteBinToHex(const char* input_file, const char* output_file);
 
 HexPair MakeHexPair(char high, char low);
 
